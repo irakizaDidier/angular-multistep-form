4b4d55e062c4fc1893bce57779c2e1c8ad35b206
irakiza44 <irakiza44@gmail.com>
2024-07-09 17:59:37 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
