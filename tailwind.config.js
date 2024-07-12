0f8dc5728c273dbb3bc184c52180a7f268c62f56
irakiza44 <irakiza44@gmail.com>
2024-07-13 00:33:07 +0200

    working on inserting responsiveness of step one page

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
       colors: {
         'Denim': '#022959',
         'error': '#ee374a',
+        'light-blue': '#bee2fd',
+        'light-gray': '#f8f9ff',
+        'border-gray': '#d6d9e6',
+        'border-blue': '#3b82f6',
       }
     },
   },
